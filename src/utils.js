a232e5f090fbf966033187e4d6ef41ca765c42d4
Lĩnh Nguyễn Hoàng <alinh1803@gmail.com>
2022-01-15 17:06:19 +0700

    updated code

--- src/utils.js
+++ src/utils.js
@@ -1,55 +1,57 @@
-import { GLTFLoader } from "./lib/three.js/examples/jsm/loaders/GLTFLoader.js";
-import { DRACOLoader } from "./lib/three.js/examples/jsm/loaders/DracoLoader.js";
-export class Utils {
-  async load3DModel(path) {
-    return new Promise(function (resolve, reject) {
-      const loaders = new GLTFLoader();
-      // const dracoLoader = new DRACOLoader();
-      // dracoLoader.setDecoderPath("/examples/js/libs/draco/");
-      // loaders.setDRACOLoader(dracoLoader);
-      loaders.load(
-        path,
-        (object) => {
-          resolve(object);
-        },
-        (xhr) => {
-          console.log(
-            "The helicopter model is " +
-              (xhr.loaded / xhr.total) * 100 +
-              "% loaded"
-          );
-        },
-        (err) => {
-          console.log("An error happened in loading helicopter model: " + err);
-          console.log(path);
-        }
-      );
-    });
-  }
+function arrayMin( array ) {
 
-  randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  }
+	if ( array.length === 0 ) return Infinity;
 
-  randomDirection(keyCode) {
-    return keyCode[this.randomNumber(0, keyCode.length)];
-  }
+	let min = array[ 0 ];
 
-  compareTwoArray(a, b) {
-    for (let i in a) {
-      if (a[i] !== b[i]) return false;
-    }
-    return true;
-  }
+	for ( let i = 1, l = array.length; i < l; ++ i ) {
 
-  loadSound() {
-    const sound = new Howl({
-      src: ["/src/music/start.mp3"],
-      // src: [
-      //   "https://redirector.googlevideo.com/videoplayback?expire=1639737653&ei=1RS8YYf3OMf9gAeNt7GIBA&ip=207.154.216.138&id=o-ADLxRuFx7sG8GJi69TqmbDeiKfpny2zqdFnRPYkcAuAa&itag=22&source=youtube&requiressl=yes&mh=sj&mm=31%2C29&mn=sn-5hneknee%2Csn-5hne6nsd&ms=au%2Crdu&mv=m&mvi=1&pl=21&initcwndbps=263750&vprv=1&mime=video%2Fmp4&cnr=14&ratebypass=yes&dur=261.061&lmt=1631007751599951&mt=1639715828&fvip=1&fexp=24001373%2C24007246&c=ANDROID&txp=5432434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Ccnr%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRQIhAJiKT7lr7Z396pEHP2W6nn-0Z1VsKu4W7zs-GJZrHNLFAiB5dXyo644gr93UWQT7XLJUVgpxYN1GX9_q7J3jXYkm6g%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRgIhAI9vV-yfiy8S6SovNjN-SY7-iMVYe5UPtcmXMxMKf5ZBAiEAvXQtun4XXd3_nEjmRh3un3oLlx4VbiYP5kpC3vW_Aro%3D&host=rr1---sn-5hneknee.googlevideo.com&title=yt5s.com-%E3%80%90Vietsub%E3%80%91Yoru%20ni%20Kakeru%20(%E5%A4%9C%E3%81%AB%E9%A7%86%E3%81%91%E3%82%8B)%20%7C%20YOASOBI",
-      // ],
-      html5: true,
-    });
-    return sound;
-  }
+		if ( array[ i ] < min ) min = array[ i ];
+
+	}
+
+	return min;
+
 }
+
+function arrayMax( array ) {
+
+	if ( array.length === 0 ) return - Infinity;
+
+	let max = array[ 0 ];
+
+	for ( let i = 1, l = array.length; i < l; ++ i ) {
+
+		if ( array[ i ] > max ) max = array[ i ];
+
+	}
+
+	return max;
+
+}
+
+const TYPED_ARRAYS = {
+	Int8Array: Int8Array,
+	Uint8Array: Uint8Array,
+	Uint8ClampedArray: Uint8ClampedArray,
+	Int16Array: Int16Array,
+	Uint16Array: Uint16Array,
+	Int32Array: Int32Array,
+	Uint32Array: Uint32Array,
+	Float32Array: Float32Array,
+	Float64Array: Float64Array
+};
+
+function getTypedArray( type, buffer ) {
+
+	return new TYPED_ARRAYS[ type ]( buffer );
+
+}
+
+function createElementNS( name ) {
+
+	return document.createElementNS( 'http://www.w3.org/1999/xhtml', name );
+
+}
+
+export { arrayMin, arrayMax, getTypedArray, createElementNS };
